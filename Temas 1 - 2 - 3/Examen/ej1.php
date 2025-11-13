53cdd9525b3b3fd65cf1c090a799aece2454205f
Antonio Cordero <antoniocorderomn@gmail.com>
2025-11-13 09:50:02 +0100

    Antonio Cordero 2DAW - ejercicio examen 13/11/25

--- Temas 1 - 2 - 3/Examen/ej1.php
+++ Temas 1 - 2 - 3/Examen/ej1.php
@@ -3,9 +3,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Examen Ejercicio 1</title>
+    <title>Examen Servidor - Antonio Cordero</title>
 </head>
 <body>
-    
+    <?php
+        //variables
+        $nombre_alumno = "Antonio Cordero Molina";
+        $modulo = "Desarrollo Web en Entorno Servidor";
+        $nota_media = 7.5;
+        $es_matriculado = true;
+
+        //impresion con echo
+        echo "<h1>" . $nombre_alumno . "</h1>";
+        echo "<h2>" . $modulo . "</h2>";
+        //impresión con comillas dobles vs comillas simples
+        echo "<p>Mi nota media actual es: $nota_media</p>";
+        echo '<p>Mi nota media actual es: $nota_media</p>';
+
+        //decisiones
+        //mensaje según nota del alumno
+        if ($nota_media >= 9) {
+            echo "<p>Sobresaliente<p>";
+        } elseif ($nota_media >= 7 && $nota_media < 9) {
+            echo "<p>Notable<p>";
+        } elseif ($nota_media >= 5 && $nota_media < 7) {
+            echo "<p>Aprobado<p>";
+        } else {
+            echo "<p>Suspenso<p>";
+        }
+
+        //mensaje según estado de matriculación
+        if ($es_matriculado) {
+            echo "<p>Estado: Alumno matriculado</p>";
+        } else {
+            echo "<p>Estado: Alumno NO matriculado</p>";
+        }
+
+        //crear tabla con bucle while
+        $filas = 0;
+        $i = 0;
+        echo "<table>";
+        echo "<tr>";
+        while ($filas<=5) {
+            echo "<td>Fila Número: </td>";
+            $filas++;
+        }
+        echo "</tr>";
+        echo "<tr>";
+        while ($i<=$filas) {
+            echo "<td>$i</td>";
+            $i++;
+        }
+        echo "</tr>";
+        echo "</table>";
+    ?>
+
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </body>
 </html>
