20172974662e812ce2dcd71630b309c142988857
Antonio Cordero <antoniocorderomn@gmail.com>
2026-01-13 13:53:37 +0100

    actividad ra4

--- 2nd_Trimestre/Tema 7/actividad_08_01_26/setup.php
+++ 2nd_Trimestre/Tema 7/actividad_08_01_26/setup.php
@@ -4,12 +4,13 @@ $host = 'localhost';
 $user = 'root';
 $pass = ''; // Dejar vacío en XAMPP por defecto
 $dbName = 'tienda';
+
 try {
     // 1. CONEXIÓN AL SERVIDOR MYSQL
     // Importante: No especificamos la base de datos en el DSN porque quizás no exista aún.
-    $pdo = new PDO("mysql:host=$host", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h3>Iniciando script de instalacion...</h3>";
+    $pdo = new PDO("mysql:host=$host", $user, $pass); // Conexión sin DB seleccionada con un sistema de objetos PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    echo "<h3>Iniciando script de instalacion...</h3>"; // Mensaje inicial de comprobación
     // 2. CREACIÓN DE LA BASE DE DATOS
     // La instrucción IF NOT EXISTS evita errores si el script se ejecuta dos veces.
     $pdo->exec("CREATE DATABASE IF NOT EXISTS $dbName CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
@@ -23,7 +24,7 @@ try {
         precio DECIMAL(10, 2) NOT NULL,
         stock INT DEFAULT 0,
         fecha_alta TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    ) ENGINE=InnoDB;"; // MOTOR DEL QUE CORRE LA VERSIÓN NUEVA 
+    ) ENGINE=InnoDB;"; // MOTOR DEL QUE CORRE LA VERSIÓN NUEVA
     $pdo->exec($sqlTabla);
     echo "[OK] Tabla 'productos' verificada.<br>";
     // 5. INSERCIÓN MASIVA DE DATOS (SEEDING)
